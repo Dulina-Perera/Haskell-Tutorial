13cba23f98fc57a8a9e86a09102ccdf0088343f5
Dulina-Perera <dulina.perera@outlook.com>
2024-04-14 13:06:05 +0530

    Learnt about variables in Haskell.

---
+++ test.hs
@@ -0,0 +1,8 @@
+main = do
+    let child = "Dudley"
+    let gifts = 36
+
+    putStrLn(child ++ " received " ++ show(gifts) ++ " gifts for his birthday.")
+    putStrLn(show(gifts) ++ " gifts? " ++ child ++ " thought out loud. But last year I had " ++ show(gifts + 1) ++"!")
+    putStrLn("This is when we knew.")
+    putStrLn(child ++ " was a very spoiled child.")
