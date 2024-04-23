ab36cdeb6ac617f817038762f35e47cbad88842d
Dulina-Perera <dulina.perera@outlook.com>
2024-04-23 08:22:46 +0530

    Haskell-Music: Played the generated raw sound wave from within the program.

--- Haskell-Music/Main.hs
+++ Haskell-Music/Main.hs
@@ -1,16 +1,38 @@
+import qualified Control.Monad
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Builder as BB
 import Data.Foldable
+import Data.Text.Array (run)
+import System.Process
+import Text.Printf
+
+outFilePath :: FilePath
+outFilePath = "wave.bin"
 
 volume :: Float
 volume = 0.5
 
+sampleCnt :: Float
+sampleCnt = 48000
+
 wave :: [Float]
-wave = map (* volume) $ map sin $ map (* step) [0..48000]
-    where step = 0.01
+wave = map (((* volume) . sin) . (* step)) [0..sampleCnt]
+    where step = 0.05
 
 save :: FilePath -> [Float] -> IO ()
 save path xs = BL.writeFile path $ BB.toLazyByteString $ foldMap BB.floatLE xs
 
+play :: Float -> IO ProcessHandle
+play audioRate = do
+    save outFilePath wave
+
+    let duration = sampleCnt / audioRate :: Float
+    runCommand $ printf "ffplay -f f32le -ar %.3f %s -nodisp -autoexit -t %.3f" audioRate outFilePath duration
+
 main :: IO ()
-main = save "wave.bin" wave
+main = do
+    let audioRate = 48000 :: Float
+    processHandle <- play audioRate
+    _ <- waitForProcess processHandle
+
+    putStrLn "Audio playback completed."
