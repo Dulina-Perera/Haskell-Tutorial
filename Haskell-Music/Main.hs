a2a8a6501c289b51ad997f5e370d309632b0d518
Dulina-Perera <dulina.perera@outlook.com>
2024-04-24 23:04:13 +0530

    Haskell-Music: Complete!

--- Haskell-Music/Main.hs
+++ Haskell-Music/Main.hs
@@ -2,11 +2,14 @@ import qualified Control.Monad
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Builder as BB
 import Data.Foldable
+import Data.List
 import Data.Text.Array (run)
 import System.Process
 import Text.Printf
 import Language.Haskell.TH.PprLib (semi)
+import Text.XHtml (rev)
 
+type Beats = Float
 type Hz = Float
 type Pulse = Float
 type Samples = Float
@@ -18,31 +21,65 @@ outFilePath = "wave.bin"
 audioRate :: Samples
 audioRate = 48000
 
+bpm :: Beats
+bpm = 120.0
+
 pitchStd :: Hz
 pitchStd = 440.0
 
 volume :: Float
 volume = 0.5
 
-freq :: Seconds -> Hz -> [Pulse]
-freq duration frequency = 
-    map (((* volume) . sin) . (* step)) [0..audioRate*duration]
-        where step = (frequency * 2 * pi) / audioRate
+freq :: Beats -> Hz -> [Pulse]
+freq beats frequency =
+    map (* volume) $ zipWith3 (\x y z -> x * y * z) attack output release
+        where
+            step = (frequency * 2 * pi) / audioRate
+            attack = map (min 1.0) [0, 0.001 ..] :: [Pulse]
+            release = reverse $ take (length output) attack
+            output = map (sin . (* step)) [0 .. audioRate * beats * (60.0 / bpm)] :: [Pulse]
 
 semiTone :: Int -> Hz
 semiTone n = pitchStd * ((2 ** (1.0 / 12.0)) ** fromIntegral n)
 
+note :: Beats -> Int -> [Pulse]
+note beats n = freq beats $ semiTone n
+
 wave :: [Pulse]
 wave = concat [
-        freq duration (semiTone 0),
-        freq duration (semiTone 2),
-        freq duration (semiTone 3),
-        freq duration (semiTone 5),
-        freq duration (semiTone 7),
-        freq duration (semiTone 8),
-        freq duration (semiTone 10),
-        freq duration (semiTone 12)
-    ] where duration = 0.5
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.5 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.5 0,
+        note 0.25 5,
+        note 0.25 5,
+        note 0.25 5,
+        note 0.25 5,
+        note 0.25 5,
+        note 0.25 5,
+        note 0.5 5,
+        note 0.25 3,
+        note 0.25 3,
+        note 0.25 3,
+        note 0.25 3,
+        note 0.25 3,
+        note 0.25 3,
+        note 0.5 3,
+        note 0.5 (-2),
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.25 0,
+        note 0.5 0
+    ]
 
 save :: FilePath -> [Float] -> IO ()
 save path xs = BL.writeFile path $ BB.toLazyByteString $ foldMap BB.floatLE xs
@@ -51,7 +88,7 @@ play :: IO ProcessHandle
 play = do
     save outFilePath wave
 
-    runCommand $ printf "ffplay -ar %.3f -autoexit -f f32le -showmode 1 -t 8.0 %s" audioRate outFilePath
+    runCommand $ printf "ffplay -ar %.3f -autoexit -f f32le -showmode 1 %s" audioRate outFilePath
 
 main :: IO ()
 main = do
