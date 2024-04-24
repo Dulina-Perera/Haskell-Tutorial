42d32b44439bef8a2179e4cf00d921cc6caaddee
Dulina-Perera <dulina.perera@outlook.com>
2024-04-24 11:14:28 +0530

    Haskell-Music: Generated semitones.

--- Haskell-Music/Main.hs
+++ Haskell-Music/Main.hs
@@ -5,6 +5,7 @@ import Data.Foldable
 import Data.Text.Array (run)
 import System.Process
 import Text.Printf
+import Language.Haskell.TH.PprLib (semi)
 
 type Hz = Float
 type Pulse = Float
@@ -17,6 +18,9 @@ outFilePath = "wave.bin"
 audioRate :: Samples
 audioRate = 48000
 
+pitchStd :: Hz
+pitchStd = 440.0
+
 volume :: Float
 volume = 0.5
 
@@ -25,8 +29,20 @@ freq duration frequency =
     map (((* volume) . sin) . (* step)) [0..audioRate*duration]
         where step = (frequency * 2 * pi) / audioRate
 
+semiTone :: Int -> Hz
+semiTone n = pitchStd * ((2 ** (1.0 / 12.0)) ** fromIntegral n)
+
 wave :: [Pulse]
-wave = concat [freq 1.0 130.82, freq 1.0 146.83, freq 1.0 164.81, freq 1.0 174.81, freq 1.0 196, freq 1.0 220, freq 1.0 246.94]
+wave = concat [
+        freq duration (semiTone 0),
+        freq duration (semiTone 2),
+        freq duration (semiTone 3),
+        freq duration (semiTone 5),
+        freq duration (semiTone 7),
+        freq duration (semiTone 8),
+        freq duration (semiTone 10),
+        freq duration (semiTone 12)
+    ] where duration = 0.5
 
 save :: FilePath -> [Float] -> IO ()
 save path xs = BL.writeFile path $ BB.toLazyByteString $ foldMap BB.floatLE xs
@@ -35,7 +51,7 @@ play :: IO ProcessHandle
 play = do
     save outFilePath wave
 
-    runCommand $ printf "ffplay -ar %.3f -autoexit -f f32le -showmode 1 -t 7.0 %s" audioRate outFilePath
+    runCommand $ printf "ffplay -ar %.3f -autoexit -f f32le -showmode 1 -t 8.0 %s" audioRate outFilePath
 
 main :: IO ()
 main = do
